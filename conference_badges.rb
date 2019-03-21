2634ac41e36351df97a8c1c10aa344d0abbba2cd
creative-zombie-4458 <jtbartlow@gmail.com>
2019-03-21 02:50:13 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,7 +4,7 @@ def badge_maker(name)
 end
 def batch_badge_creator(attendees)
   badges = []
-  names.each {|attendee| badges << badge_maker(name) }
+  .each {|attendee| badges << badge_maker(name) }
     badges
   end
   
